4d9e53a0b0d1a1c3e8d11f03e3ea08a8b7955300
bbenamara <bouabdallah.benamara@sqits.net>
2021-05-05 11:23:58 +0200

    Initial commit

---
+++ e_pasokon/product/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+from product import views
+
+urlpatterns =  [
+    path('latest-products/', views.LatestProductList.as_view()),
+    path('product/<slug:category_slug>/<slug:product_slug>/', views.ProductDetail.as_view()),
+]
